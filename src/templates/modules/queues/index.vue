a4b58f99711966d4a6210596a5e68cbef77bee46
MattIPv4 <me@mattcowley.co.uk>
2020-01-07 17:58:53 +0000

    Add unanswered questions count

--- src/templates/modules/queues/index.vue
+++ src/templates/modules/queues/index.vue
@@ -1,5 +1,5 @@
 <!--
-Copyright 2019 DigitalOcean
+Copyright 2020 DigitalOcean
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -16,7 +16,7 @@ limitations under the License.
 
 <template>
     <div v-if="app.$data.state === 'home'" class="dmt-queues">
-        <a v-for="queue in queues" class="dmt-button" :href="queue.link">{{ queue.text }}</a>
+        <a v-for="queue in Object.values(queues)" class="dmt-button" :href="queue.link">{{ queue.text }}</a>
     </div>
 </template>
 
@@ -30,14 +30,45 @@ limitations under the License.
         };
     };
 
-    const queues = [
-        getQueue('/community/moderation/under_review?moderation=true'),
-        getQueue('/community/moderation/flagged?moderation=true'),
-        {
+    const queues = {
+        underReview: getQueue('/community/moderation/under_review?moderation=true'),
+        flagged: getQueue('/community/moderation/flagged?moderation=true'),
+        unansweredQuestions: {
             link: '/community/questions?primary_filter=unanswered',
             text: 'Unanswered Questions',
         },
-    ];
+    };
+
+    const encodeQueryData = data => {
+        const ret = [];
+        for (let d in data)
+            ret.push(encodeURIComponent(d) + '=' + encodeURIComponent(data[d]));
+        return ret.join('&');
+    };
+
+    const getUnanswered = async () => {
+        const applicationId = '6ZHEUVKJ88';
+        const apiKey = 'c5470567eae7fa1177d43222e18ba086';
+        const index = 'production_community_consolidated_newest';
+        const resp = await fetch(`https://${applicationId.toLowerCase()}-dsn.algolia.net/1/indexes/${index}/query?x-algolia-application-id=${applicationId}&x-algolia-api-key=${apiKey}`, {
+            method: 'POST',
+            body: JSON.stringify({
+                params: encodeQueryData({
+                    query: '',
+                    page: 0,
+                    hitsPerPage: 0,
+                    facetFilters: '["feedable_language:en","item_type:question"]',
+                    numericFilters: '["indexed_answers_count=0"]',
+                }),
+            }),
+        });
+
+        return (await resp.json()).nbHits;
+    };
+
+    getUnanswered().then(res => {
+        queues.unansweredQuestions.text += ` (${res.toLocaleString()})`;
+    });
 
     module.exports = {
         name: 'Queues',
