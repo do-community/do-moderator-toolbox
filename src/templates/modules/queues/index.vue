4e5254cf5ef0d2add8743add0737bb5aafedfca3
MattIPv4 <me@mattcowley.co.uk>
2020-03-02 19:40:02 +0000

    Remove old modbar based queue logic

--- src/templates/modules/queues/index.vue
+++ src/templates/modules/queues/index.vue
@@ -21,24 +21,6 @@ limitations under the License.
 </template>
 
 <script>
-    const getQueue = href => {
-        const elm = document.querySelector(`.moderation-bar li a[href="${href}"]`);
-        if (!elm) return;
-        return {
-            link: href,
-            text: elm.textContent,
-        };
-    };
-
-    const queues = {
-        underReview: getQueue('/community/moderation/under_review?moderation=true'),
-        flagged: getQueue('/community/moderation/flagged?moderation=true'),
-        unansweredQuestions: {
-            link: '/community/questions?secondary_filter=unanswered',
-            text: 'Unanswered Questions',
-        },
-    };
-
     const encodeQueryData = data => {
         const ret = [];
         for (let d in data)
@@ -46,29 +28,42 @@ limitations under the License.
         return ret.join('&');
     };
 
-    const getUnanswered = async () => {
-        const applicationId = '6ZHEUVKJ88';
-        const apiKey = 'c5470567eae7fa1177d43222e18ba086';
-        const index = 'production_community_consolidated_newest';
-        const resp = await fetch(`https://${applicationId.toLowerCase()}-dsn.algolia.net/1/indexes/${index}/query?x-algolia-application-id=${applicationId}&x-algolia-api-key=${apiKey}`, {
-            method: 'POST',
-            body: JSON.stringify({
-                params: encodeQueryData({
-                    query: '',
-                    page: 0,
-                    hitsPerPage: 0,
-                    facetFilters: '["feedable_language:en","item_type:question"]',
-                    numericFilters: '["indexed_answers_count=0"]',
-                }),
-            }),
-        });
+    const queues = {
+        underReview: {
+            link: '/community/moderation/under_review?moderation=true',
+            text: 'Under Review',
+            count: null,
+        },
+        flagged: {
+            link: '/community/moderation/flagged?moderation=true',
+            text: 'Flagged',
+            count: null,
+        },
+        unansweredQuestions: {
+            link: '/community/questions?secondary_filter=unanswered',
+            text: 'Unanswered Questions',
+            count: null,
+            async getCount() {
+                const applicationId = '6ZHEUVKJ88';
+                const apiKey = 'c5470567eae7fa1177d43222e18ba086';
+                const index = 'production_community_consolidated_newest';
+                const resp = await fetch(`https://${applicationId.toLowerCase()}-dsn.algolia.net/1/indexes/${index}/query?x-algolia-application-id=${applicationId}&x-algolia-api-key=${apiKey}`, {
+                    method: 'POST',
+                    body: JSON.stringify({
+                        params: encodeQueryData({
+                            query: '',
+                            page: 0,
+                            hitsPerPage: 0,
+                            facetFilters: '["feedable_language:en","item_type:question"]',
+                            numericFilters: '["indexed_answers_count=0"]',
+                        }),
+                    }),
+                });
 
-        return (await resp.json()).nbHits;
+                return (await resp.json()).nbHits;
+            },
+        },
     };
-
-    getUnanswered().then(res => {
-        queues.unansweredQuestions.text += ` (${res.toLocaleString()})`;
-    });
 
     module.exports = {
         name: 'Queues',
