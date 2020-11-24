00e45dccc792bf93a5a4b425d8110d1a82b2a30b
MattIPv4 <me@mattcowley.co.uk>
2020-11-24 18:47:55 +0000

    Batch user post requests to remain below ratelimit

--- src/utils/user/getUserPosts.js
+++ src/utils/user/getUserPosts.js
@@ -25,22 +25,44 @@ const fetchPage = async (userId, page) => {
     return await resp.json().catch(() => {});
 };
 
+const processBatch = async (requests, posts) => {
+    const results = await Promise.all(requests);
+    for (const data of results) {
+        if (data && data.data) posts.push(...data.data);
+    }
+};
+
 module.exports = async userId => {
     // Make the first request to get meta data
     const allPosts = [];
     const initial = await fetchPage(userId, 1).catch(() => {});
     if (initial && initial.data) allPosts.push(...initial.data);
 
-    // If there are more pages, do them all at the same time
+    // If there are more pages, fetch them all in batches
     if (initial && initial.meta && initial.meta['total-pages'] && initial.meta['total-pages'] > 1) {
-        const requests = [];
+        let requests = [];
+        let start = Date.now();
         for (let i = 2; i <= initial.meta['total-pages']; i++) {
             requests.push(fetchPage(userId, i));
+
+            // Batch requests into chunks of 5
+            if ((i - 1) % 5 === 0) {
+                console.log(`Fetching ${i}... total pages ${initial.meta['total-pages']}`);
+                await processBatch(requests, allPosts);
+
+                // Add a delay, aiming for 1rps
+                const delay = Math.max((5 * 1000) - (Date.now() - start));
+                console.log(`Waiting for ${delay}ms before continuing fetching`);
+                await new Promise(r => setTimeout(r, delay));
+
+                // Reset for next batch
+                requests = [];
+                start = Date.now();
+            }
         }
-        const results = await Promise.all(requests);
-        for (const data of results) {
-            if (data && data.data) allPosts.push(...data.data);
-        }
+
+        // Handle any in the final batch
+        await processBatch(requests, allPosts);
     }
 
     // Sort by date
